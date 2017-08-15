c246366a3307e3eeecd641990818431d6415fdd0
Mattias Wennmo <wennmo@users.noreply.github.com>
2017-08-15 10:52:15 +0200

    Add examples for basic lists (#187)
    * Add examples for basic lists

---
+++ examples/basics/lists/app-object-list.js
@@ -0,0 +1,46 @@
+const createSession = require('../../session');
+
+const properties = {
+  qInfo: {
+    qType: 'my-list',
+  },
+  qAppObjectListDef: {
+    qType: 'my-object',
+    qData: {
+      title: '/meta/title',
+    },
+  },
+};
+
+const session = createSession();
+
+// Open the session and create a session document:
+session
+  .open()
+  .then(global => global.createSessionApp())
+  .then((doc) => {
+    const tasks = [];
+
+    // Create 10 objects of type my-object with unique titles
+    for (let i = 0; i < 10; i += 1) {
+      tasks.push(
+        doc.createObject({
+          qInfo: {
+            qType: 'my-object',
+          },
+          meta: {
+            title: `my-object${i}`,
+          },
+        }));
+    }
+
+    // Create a app object list using qAppObjectListDef and list all objects of type my-object
+    // and also lists the title for each object.
+    return Promise.all(tasks).then(() => // eslint-disable-line no-restricted-globals
+      doc
+        .createObject(properties)
+        .then(object => object.getLayout())
+        .then(layout => console.log('App object list:', JSON.stringify(layout, null, '  ')))
+        .then(() => session.close()));
+  })
+  .catch(error => console.log('Session: Failed to open socket:', error));
---
+++ examples/basics/lists/field-list.js
@@ -0,0 +1,37 @@
+const createSession = require('../../session');
+
+const script = `
+TempTable:
+Load
+RecNo() as Field1,
+Rand() as Field2,
+Rand() as Field3
+AutoGenerate 100
+`;
+
+const properties = {
+  qInfo: {
+    qType: 'my-field-list',
+  },
+  qFieldListDef: {},
+};
+
+const session = createSession();
+
+// Open the session and create a session document:
+session
+  .open()
+  .then(global => global.createSessionApp())
+  .then(doc =>
+    // Load in some data into the session document:
+    doc.setScript(script)
+      .then(() => doc.doReload())
+      .then(() =>
+        // Create a field list using qFieldListDef and list all fields available in the document.
+        doc
+          .createObject(properties)
+          .then(object => object.getLayout())
+          .then(layout => console.log('Field list:', JSON.stringify(layout, null, '  ')))
+          .then(() => session.close())))
+  .catch(error => console.log('Session: Failed to open socket:', error));
+
