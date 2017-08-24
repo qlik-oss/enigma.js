38802aca747e31b0022f6eda8da1815e7adc6391
Marcus Offesson <marcusoffesson@users.noreply.github.com>
2017-08-24 15:16:12 +0200

    Integration tests: Use session apps (#216)

--- test/integration/sugar-mixins.spec.js
+++ test/integration/sugar-mixins.spec.js
@@ -7,68 +7,64 @@ const HIGH_TIMEOUT = 15000;
 describe('Sugar mixins', () => {
   let qixGlobal;
 
-  before(() => {
-    const config = utils.getDefaultConfig();
-    config.mixins = [{
-      types: 'Global',
-      extend: {
-        createAppAndLoad(appName, loadScript) {
-          return qixGlobal.createApp(appName).then(appInfo =>
-            qixGlobal.openDoc(appInfo.qAppId).then(app =>
-              app.setScript(loadScript).then(() =>
-                app.doReload(0, false, false).then(() => ({ app, appInfo })),
-              ),
-            ).catch(err =>
-              qixGlobal.deleteApp(appInfo.qAppId).then(() => {
-                throw err;
-              }),
-            ),
+  const globalMixin = {
+    types: 'Global',
+    extend: {
+      prepareSessionApp(script) {
+        let app;
+        return this.createSessionApp()
+          .then((a) => { app = a; })
+          .then(() => app.setScript(script))
+          .then(() => app.doReload(0, false, false))
+          .then(() => app);
+      },
+    },
+  };
+
+  const docMixin = {
+    types: 'Doc',
+    extend: {
+      getList(listDef) {
+        return this.getObject(listDef.qInfo.qId)
+          .catch(() => this.createSessionObject(listDef))
+          .then(obj =>
+            obj.getLayout(),
           );
-        },
       },
-    }, {
-      types: 'Doc',
-      extend: {
-        getList(listDef) {
-          return this.getObject(listDef.qInfo.qId)
-            .catch(() => this.createSessionObject(listDef))
-            .then(obj =>
-              obj.getLayout(),
-            );
-        },
-        createSheet(title, description, thumbnail) {
-          return this.createObject({
-            qInfo: {
-              qType: 'sheet',
+      createSheet(title, description, thumbnail) {
+        return this.createObject({
+          qInfo: {
+            qType: 'sheet',
+          },
+          qMetaDef: {
+            title: title || '',
+            description: description || '',
+          },
+          rank: -1,
+          thumbnail: { qStaticContentUrlDef: thumbnail || null },
+          columns: 24,
+          rows: 12,
+          cells: [],
+          qChildListDef: {
+            qData: {
+              title: '/title',
             },
-            qMetaDef: {
-              title: title || '',
-              description: description || '',
-            },
-            rank: -1,
-            thumbnail: { qStaticContentUrlDef: thumbnail || null },
-            columns: 24,
-            rows: 12,
-            cells: [],
-            qChildListDef: {
-              qData: {
-                title: '/title',
-              },
-            },
-          });
-        },
+          },
+        });
       },
-    }];
+    },
+  };
+
+  beforeEach(() => {
+    const config = utils.getDefaultConfig();
+    config.mixins = [globalMixin, docMixin];
 
     return Qix.create(config).open().then((global) => {
       qixGlobal = global;
     });
   });
 
-  after(() => {
-    qixGlobal.session.on('error', () => {}); // Swallow the error
-    return qixGlobal.session.close();
-  });
+  afterEach(() => qixGlobal.session.close());
 
   it('should create and list sheets', () => {
     const listDef = {
@@ -88,27 +84,22 @@ describe('Sugar mixins', () => {
         },
       },
     };
-    const appName = 'myAppName1';
     const sheetName = 'mySheetName';
 
-    return qixGlobal.createApp(appName).then(appInfo =>
-      qixGlobal.openDoc(appInfo.qAppId).then(app =>
-        app.getList(listDef).then((list) => {
-          expect(list.qAppObjectList.qItems.length).to.equal(0);
-          return app.createSheet(sheetName).then(sheet =>
-            app.getList(listDef).then((listAfter) => {
-              expect(listAfter.qAppObjectList.qItems.length).to.equal(1);
-              expect(listAfter.qAppObjectList.qItems[0].qInfo.qId).to.equal(sheet.id);
-              return qixGlobal.deleteApp(appInfo.qAppId);
-            }),
-          );
-        }),
-      ).catch(err =>
-        qixGlobal.deleteApp(appInfo.qAppId).then(() => {
-          throw err;
-        }),
-      ),
-    );
+    let app;
+    let sheet;
+
+    return qixGlobal.createSessionApp()
+      .then((a) => { app = a; })
+      .then(() => app.getList(listDef))
+      .then(list => expect(list.qAppObjectList.qItems.length).to.equal(0))
+      .then(() => app.createSheet(sheetName))
+      .then((s) => { sheet = s; })
+      .then(() => app.getList(listDef))
+      .then((list) => {
+        expect(list.qAppObjectList.qItems.length).to.equal(1);
+        expect(list.qAppObjectList.qItems[0].qInfo.qId).to.equal(sheet.id);
+      });
   }).timeout(HIGH_TIMEOUT);
 
   it('should load script and make selections', () => {
@@ -118,24 +109,14 @@ describe('Sugar mixins', () => {
       },
       qSelectionObjectDef: {},
     };
-    const appName = 'myAppName2';
+    let app;
 
-    return qixGlobal.createAppAndLoad(appName, ctrl00).then((obj) => {
-      const app = obj.app;
-      const appInfo = obj.appInfo;
-      return app.getField('Alpha').then(field =>
-        field.selectAll().then(() =>
-          app.getList(listDef).then((list) => {
-            expect(list.qSelectionObject.qSelections[0].qSelectedCount).to.equal(26);
-            return qixGlobal.deleteApp(appInfo.qAppId);
-          }),
-        ),
-      ).catch(err =>
-        qixGlobal.deleteApp(appInfo.qAppId).then(() => {
-          throw err;
-        }),
-      );
-    });
+    return qixGlobal.prepareSessionApp(ctrl00)
+      .then((a) => { app = a; })
+      .then(() => app.getField('Alpha'))
+      .then(field => field.selectAll())
+      .then(() => app.getList(listDef))
+      .then(list => expect(list.qSelectionObject.qSelections[0].qSelectedCount).to.equal(26));
   }).timeout(HIGH_TIMEOUT);
 
   it('should create and list measures', () => {
@@ -165,24 +146,15 @@ describe('Sugar mixins', () => {
         qDef: 'sum(Num)',
       },
     };
-    const appName = 'myAppName3';
+    let app;
+    let measure;
 
-    return qixGlobal.createAppAndLoad(appName, ctrl00).then((obj) => {
-      const app = obj.app;
-      const appInfo = obj.appInfo;
-      return app.createMeasure(measureDef).then(measure =>
-        app.getList(listDef).then((list) => {
-          const createdMeasureId = measure.id;
-          const listMeasure = list.qMeasureList.qItems[0].qInfo.qId;
-          expect(createdMeasureId).to.equal(listMeasure);
-          return qixGlobal.deleteApp(appInfo.qAppId);
-        }),
-      ).catch(err =>
-        qixGlobal.deleteApp(appInfo.qAppId).then(() => {
-          throw err;
-        }),
-      );
-    });
+    return qixGlobal.prepareSessionApp(ctrl00)
+      .then((a) => { app = a; })
+      .then(() => app.createMeasure(measureDef))
+      .then((m) => { measure = m; })
+      .then(() => app.getList(listDef))
+      .then(list => expect(measure.id).to.equal(list.qMeasureList.qItems[0].qInfo.qId));
   }).timeout(HIGH_TIMEOUT);
 
   it('should load script and list fields', () => {
@@ -197,23 +169,11 @@ describe('Sugar mixins', () => {
         qShowSrcTables: true,
       },
     };
-    const appName = 'myAppName4';
 
-    return qixGlobal.createAppAndLoad(appName, ctrl00).then((obj) => {
-      const app = obj.app;
-      const appInfo = obj.appInfo;
-      return app.getList(listDef).then((list) => {
-        const userFields = list.qFieldList.qItems.filter(qField =>
-          qField.qIsHidden !== true,
-        );
-        expect(userFields.length).to.equal(12);
-        return qixGlobal.deleteApp(appInfo.qAppId);
-      }).catch(err =>
-        qixGlobal.deleteApp(appInfo.qAppId).then(() => {
-          throw err;
-        }),
-      );
-    });
+    return qixGlobal.prepareSessionApp(ctrl00)
+      .then(app => app.getList(listDef))
+      .then(list => list.qFieldList.qItems.filter(qField => qField.qIsHidden !== true))
+      .then(userFields => expect(userFields.length).to.equal(12));
   }).timeout(HIGH_TIMEOUT);
 
   it('should create and list dimensions', () => {
@@ -251,24 +211,15 @@ describe('Sugar mixins', () => {
         tags: [],
       },
     };
-    const appName = 'myAppName5';
+    let app;
+    let dimension;
 
-    return qixGlobal.createAppAndLoad(appName, ctrl00).then((obj) => {
-      const app = obj.app;
-      const appInfo = obj.appInfo;
-      return app.createDimension(dimensionDef).then(dimension =>
-        app.getList(listDef).then((list) => {
-          const createdDimensionId = dimension.id;
-          const listDiemnsion = list.qDimensionList.qItems[0].qInfo.qId;
-          expect(createdDimensionId).to.equal(listDiemnsion);
-          return qixGlobal.deleteApp(appInfo.qAppId);
-        }),
-      ).catch(err =>
-        qixGlobal.deleteApp(appInfo.qAppId).then(() => {
-          throw err;
-        }),
-      );
-    });
+    return qixGlobal.prepareSessionApp(ctrl00)
+      .then((a) => { app = a; })
+      .then(() => app.createDimension(dimensionDef))
+      .then((d) => { dimension = d; })
+      .then(() => app.getList(listDef))
+      .then(list => expect(dimension.id).to.equal(list.qDimensionList.qItems[0].qInfo.qId));
   }).timeout(HIGH_TIMEOUT);
 
   it('should list created Variable', () => {
@@ -293,25 +244,15 @@ describe('Sugar mixins', () => {
       qComment: '',
       qDefinition: 'Month',
     };
-    const appName = 'myAppName6';
+    let app;
+    let variable;
 
-    return qixGlobal.createAppAndLoad(appName, ctrl00).then((obj) => {
-      const app = obj.app;
-      const appInfo = obj.appInfo;
-      return app.createVariableEx(variableDef).then(variable =>
-        app.getList(listDef).then((list) => {
-          const createdVariableId = variable.id;
-          const userVariables = list.qVariableList.qItems.filter(qVariable =>
-            qVariable.qIsReserved !== true,
-          );
-          expect(userVariables[0].qInfo.qId).to.equal(createdVariableId);
-          return qixGlobal.deleteApp(appInfo.qAppId);
-        }),
-      ).catch(err =>
-        qixGlobal.deleteApp(appInfo.qAppId).then(() => {
-          throw err;
-        }),
-      );
-    });
+    return qixGlobal.prepareSessionApp(ctrl00)
+      .then((a) => { app = a; })
+      .then(() => app.createVariableEx(variableDef))
+      .then((v) => { variable = v; })
+      .then(() => app.getList(listDef))
+      .then(list => list.qVariableList.qItems.filter(v => v.qIsReserved !== true))
+      .then(userVariables => expect(userVariables[0].qInfo.qId).to.equal(variable.id));
   }).timeout(HIGH_TIMEOUT);
 });
