2dfb624217ce9c23be4e3bbcd607581fa4b10125
Marcus Offesson <marcusoffesson@users.noreply.github.com>
2017-09-21 15:42:01 +0200

    Refactored handleObjectReferenceResponse into a response interceptor (#242)

---
+++ src/interceptors/response/api.js
@@ -0,0 +1,21 @@
+/**
+* Response interceptor for generating APIs. Handles the quirks of engine not
+* returning an error when an object is missing.
+* @param {Object} session - The session the intercept is being executed on.
+* @param {Object} request - The JSON-RPC request.
+* @param {Object} response - The response.
+* @returns {Object} - Returns the generated API
+*/
+export default function apiInterceptor(session, request, response) {
+  if (response.qHandle && response.qType) {
+    return session.getObjectApi({
+      handle: response.qHandle,
+      type: response.qType,
+      id: response.qGenericId,
+      genericType: response.qGenericType,
+    });
+  } else if (response.qHandle === null && response.qType === null) {
+    throw new Error('Object not found');
+  }
+  return response;
+}
