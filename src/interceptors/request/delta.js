febee2598076da749f44e8e8724e5b092548368e
Marcus Offesson <mnf@qlik.com>
2017-10-24 09:52:15 +0200

    Minor style change

--- src/interceptors/request/delta.js
+++ src/interceptors/request/delta.js
@@ -2,7 +2,8 @@ const SUCCESS_KEY = 'qSuccess';
 
 export default function deltaRequestInterceptor(session, request) {
   const delta =
-    session.config.protocol.delta && request.outKey !== -1 &&
+    session.config.protocol.delta &&
+    request.outKey !== -1 &&
     request.outKey !== SUCCESS_KEY;
   if (delta) {
     request.delta = delta;
