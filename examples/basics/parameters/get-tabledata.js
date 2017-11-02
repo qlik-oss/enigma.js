a478670d6645b2d253f36fba05e41f8922dd2370
Thomas Hänig <thomas.haenig@akquinet.de>
2017-11-02 11:17:18 +0100

    fixing lint issues
    Signed-off-by: Thomas Hänig <thomas.haenig@akquinet.de>

--- examples/basics/parameters/get-tabledata.js
+++ examples/basics/parameters/get-tabledata.js
@@ -4,11 +4,11 @@ const createSession = require('../../session');
 const script = `
 exampletable:
 load * Inline [
-	dim, val
-	foo, 23
-	bar, 42
-	c, 54
-	d, 21
+dim, val
+foo, 23
+bar, 42
+c, 54
+d, 21
 ];
 `;
 
@@ -16,56 +16,54 @@ const session = createSession();
 
 /**
  * Write the tabelContent to console.
- * @param {*} tabelContent 
+ * @param {*} tabelContent
  */
 function print(tableContent) {
-    for(let i = 0; i<tableContent.length; i++) {        
-        console.log("dim:" + tableContent[i].qValue[0].qText + ' val: ' + tableContent[i].qValue[1].qText);
-    }
+  for (let i = 0; i < tableContent.length; i += 1) {
+    console.log('dim: ${tableContent[i].qValue[0].qText} val: ${tableContent[i].qValue[1].qText}');
+  }
 }
 
 // local variable to store the generated sessionapp
 let sessionApp;
 
 session.open()
-    .then((global) => {
-        console.log("Session opend");
-        return global.createSessionApp()
-    })
-    .then((app) => {
-        console.log("App generated");
-        sessionApp = app;
-        return app.setScript(script)
-    })
-    .then(() => {
-        console.log("Script recieved");
-        return sessionApp.doReload();
-    })
-    .then(() => {
-        return Promise.all(
-            [
-                sessionApp.getTableData(0, 4, false, 'exampletable')
-                    .then((tableContent)=>{ 
-                        console.log('Position based');
-                        print(tableContent);
-                    }),
-                    
-                sessionApp.getTableData({
-                    qOffset: 0,
-                    qRows: 4,
-                    qSyntheticMode: false,
-                    qTableName: 'exampletable'
-                })
-                    .then((tableContent)=>{ 
-                        console.log('Name based');
-                        print(tableContent)
-                    })
-            ]);
-    })
-    .then(() => {
-        console.log("Session will be closed after every call is done");
-        session.close();
-    })
-    .catch((error) => {
-        console.error("Error", error);
-    });
+  .then((global) => {
+    console.log('Session opend');
+    return global.createSessionApp();
+  })
+  .then((app) => {
+    console.log('App generated');
+    sessionApp = app;
+    return app.setScript(script);
+  })
+  .then(() => {
+    console.log('Script recieved');
+    return sessionApp.doReload();
+  })
+  .then(() => {
+    return Promise.all([
+      sessionApp.getTableData(0, 4, false, 'exampletable')
+        .then((tableContent) => {
+          console.log('Position based');
+          print(tableContent);
+        }),
+      sessionApp.getTableData({
+        qOffset: 0,
+        qRows: 4,
+        qSyntheticMode: false,
+        qTableName: 'exampletable'
+      })
+        .then((tableContent)=>{ 
+          console.log('Name based');
+          print(tableContent)
+        })
+    ]);
+  })
+  .then(() => {
+      console.log('Session will be closed after every call is done');
+    session.close();
+  })
+  .catch((error) => {
+    console.error('Error', error);
+  });
