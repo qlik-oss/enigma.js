6c36aadca52b4a6636caa3d88a408b5358da6ad5
Miralem Drek <mek@qlik.com>
2019-10-28 13:57:31 +0100

    docs: document enigma error

--- src/error.js
+++ src/error.js
@@ -1,11 +1,24 @@
+
+/**
+ * Error containing a custom error code.
+ * @extends Error
+ * @property {number} code The error code
+ * @property {boolean} enigmaError=true
+ */
+class EnigmaError extends Error {
+  constructor(name, code) {
+    super(name);
+    this.code = code;
+    this.enigmaError = true;
+  }
+}
+
 /**
  * Create an enigmaError
  * @param {Number} code A proper error code from ../error-codes.js
  * @param {String} name A message/name of the enigmaError.
+ * @returns {EnigmaError}
  */
 export default function createEnigmaError(code, name) {
-  const error = new Error(name);
-  error.code = code;
-  error.enigmaError = true; // To be able to properly identify an enigma error
-  return error;
+  return new EnigmaError(name, code);
 }
